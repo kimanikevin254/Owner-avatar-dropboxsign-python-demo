b890fddc8b89c82275025b5efde3b40a3cd5a245
Kevin Kim <58376469+kimanikevin254@users.noreply.github.com>
2024-06-26 19:46:49 +0300

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,4 @@
-from dropbox_sign import ApiClient, ApiException, Configuration, apis, models
 import os
-
-configuration = Configuration( username = os.getenv('DS_API_KEY') )
 
 # Create an Embedded Signature Request
 def create_embedded_signature_request(title, description, signers, filepath):
